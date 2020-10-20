0eecb11db19f5903cbc5fa5251c8bad74b25caf7
fviramontes8 <kuolemajarvi@yahoo.com>
2020-10-19 18:36:26 -0600

    Added finished program on linear search

--- Search/LinearSearch/main.cpp
+++ Search/LinearSearch/main.cpp
@@ -22,27 +22,36 @@ int linear_search_index(const std::vector<X> &v, X value) {
 	return -1;
 }
 
+template <typename Q>
+bool linear_search_ptr(const std::vector<Q> &v, Q value) {
+	for (auto i = v.begin(); i != v.end(); i++) {
+		if (*i == value) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
 	std::vector<float> f = { 3.8, 9.1, 5.7, 2.3, 4.0 };
 	std::vector<std::string> s = { "borderlands", "cthulu", "spyro", "megadeth" };
 
-	float search1 = 42.0, search2 = 2.3;
-	std::string search3 = "hi", search4 = "spyro";
+	float f_search1 = 9.0, f_search2 = 4.0;
+	std::string s_search1 = "borderlands", s_search2 = "nyarlathotep";
 
-	std::cout << "True or false:\n";
+	std::cout << "Linear search bool:\n";
+	std::cout << linear_search(f, f_search1) << '\t';
+	std::cout << linear_search(f, f_search2) << '\n';
 
-	std::cout << linear_search(f, search1) << '\t';
-	std::cout << linear_search(f, search2) << '\n';
-	std::cout << linear_search(s, search3) << '\t';
-	std::cout << linear_search(s, search4) << '\n';
+	std::cout << linear_search(s, s_search1) << '\t';
+	std::cout << linear_search(s, s_search2) << '\n';
 
-	std::cout << "Search for index:\n";
+	std::cout << "Linear search index:\n";
+	std::cout << linear_search_index(f, f_search1) << '\t';
+	std::cout << linear_search_index(f, f_search2) << '\n';
 
-	std::cout << linear_search_index(f, search1);
-	std::cout << '\t' << linear_search_index(f, search2) << '\n';
-
-	std::cout << linear_search_index(s, search3) << '\t';
-	std::cout << linear_search_index(s, search4) << '\n';
+	std::cout << linear_search_index(s, s_search1) << '\t';
+	std::cout << linear_search_index(s, s_search2) << '\n';
 
 	return 0;
 }
