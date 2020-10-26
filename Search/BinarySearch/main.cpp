91c56c50443398d999b7f418195e3703636e3b25
fviramontes8 <kuolemajarvi@yahoo.com>
2020-10-25 23:58:12 -0600

    Fixed error in binary_search

--- Search/BinarySearch/main.cpp
+++ Search/BinarySearch/main.cpp
@@ -6,7 +6,7 @@ bool binary_search(const std::vector<T> &v, T value) {
 	int begin = 0;
 	int end = v.size() - 1;
 	while(true) {
-		int mid = (begin+end) / 2.0;
+		int mid = (begin + end) / 2;
 		if (v[mid] == value) {
 			return true;
 		}
@@ -18,7 +18,7 @@ bool binary_search(const std::vector<T> &v, T value) {
 			begin = mid + 1;
 		}
 
-		if (mid == end) {
+		if ((begin == mid) || (end == mid)) {
 			return false;
 		}
 	}
@@ -28,7 +28,7 @@ bool binary_search(const std::vector<T> &v, T value) {
 int main() {
 	std::vector<int> x = {2, 5, 7, 12, 16, 20, 22};
 
-	int search_1 = 20, search_2 = 21;
+	int search_1 = 20, search_2 = 25;
 
 	std::cout << binary_search(x, search_1) << '\t';
 	std::cout << binary_search(x, search_2) << '\n';
